a6a3f9c697f6e8de0b28655da33573ebc2b11b35
task0654 <2807259014@qq.com>
2023-07-19 04:44:24 +0800

    Function cleanup
    Signed-off-by: task0654 <2807259014@qq.com>

--- config/common.mk
+++ config/common.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include vendor/aosp/config/version.mk
+# include vendor/aosp/config/version.mk
 
 PRODUCT_PACKAGES += \
     NetworkStackOverlay
