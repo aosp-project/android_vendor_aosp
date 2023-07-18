ef6de90c812e3952eb1e55b2cad5b860902aa485
task0654 <2807259014@qq.com>
2023-07-18 23:56:08 +0800

    build: tasks: bacon.mk: aosp change
    Signed-off-by: task0654 <2807259014@qq.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -1,36 +1,24 @@
 #
-# Copyright (C) 2018-2023 ArrowOS
+# Copyright (C) 2018-2023 Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-ARROW_TARGET_PACKAGE := $(PRODUCT_OUT)/$(ARROW_VERSION).zip
+AOSP_TARGET_PACKAGE := $(PRODUCT_OUT)/$(AOSP_VERSION).zip
 
 .PHONY: bacon
 bacon: otapackage
-	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(ARROW_TARGET_PACKAGE)
-	$(hide) $(MD5SUM) $(ARROW_TARGET_PACKAGE) > $(ARROW_TARGET_PACKAGE).md5sum
+	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(AOSP_TARGET_PACKAGE)
+	$(hide) $(MD5SUM) $(AOSP_TARGET_PACKAGE) > $(AOSP_TARGET_PACKAGE).md5sum
 	echo -e "========================================="
 	echo -e ""
-	echo -e "                 \033[1;34m''\033[0m                  "
-	echo -e "           \`\`\`\`\`\033[1;34m.oy.\033[0m\`\`\`\`\`\`\`          "
-	echo -e "        \`\`\`\`\`\`\`\033[1;34m:dMMh.\033[0m\`\`\`\`\`\`\`\`        "
-	echo -e "      \`\`\`\`\`\`\`\033[1;34m.sMMMMMy.\033[0m\`\`\`\`\`\`\`\`\`      "
-	echo -e "    \`\`\`\`\`\`\`\033[1;34m./mMMMMMMMy.\033[0m\`\`\`\`\`\`\`\`\`\`    "
-	echo -e "   \`\`\`\`\`\`\`\033[1;34m-yMMMMMMMMMMy.\033[0m\`\`\`\`\`\`\`\`\`\`   "
-	echo -e "   \`\`\`\`\`\033[1;34m./mMMNNNMMMMMMMy.\033[0m\`\`\`\`\`\`\`\`\`\`  "
-	echo -e "  \`\`\`\`\`\033[1;34m-ymmhMMMMMMhNMMMMs.\033[0m\`\`\`\`\`\`\`\`\`  "
-	echo -e "  \`\`\`\`\033[1;34m./M..\033[0m\`\`\`\`\`\`\`\033[1;34m.-odMMMs.\033[0m\`\`\`\`\`\`\`\`  "
-	echo -e "  \`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\033[1;34m./hMMs.\033[0m\`\`\`\`\`\`\`  "
-	echo -e "   \`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\033[1;34m./dMs.\033[0m\`\`\`\`\`\`  "
-	echo -e "    \`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\033[1;34m.+ms\033[0m\`\`\`\`\`  "
-	echo -e "     \`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\033[1;34m-yo\033[0m\`\`\`   "
-	echo -e "      \`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\033[1;34m...d\033[0m\`    "
-	echo -e "        \`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\` \`\` \033[1;34m.   \033[0m"
-	echo -e "           \`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`          "
-	echo -e ""
-	echo -e "                ArrowOS"
+	echo -e	"		 __ _  ___  ___ _ __  
+ 	echo -e "		/ _` |/ _ \/ __| '_ \ 
+	echo -e	"	    | (_| | (_) \__ \ |_) |
+ 	echo -e	"		\__,_|\___/|___/ .__/ 
+	echo -e "               		|_|
+	echo -e	"
 	echo -e ""
 	echo -e "========================================="
 	echo -e ""
-	@echo "**Package completed**: $(ARROW_TARGET_PACKAGE)"
+	@echo "**Package completed**: $(AOSP_TARGET_PACKAGE)"
