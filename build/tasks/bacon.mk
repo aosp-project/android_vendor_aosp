fdec27743bbe38026f66e7babca7090be56a87a4
task0654 <2807259014@qq.com>
2023-07-19 08:32:39 +0800

    build: Fix: missing separator
    Signed-off-by: task0654 <2807259014@qq.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -11,14 +11,11 @@ bacon: otapackage
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(AOSP_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(AOSP_TARGET_PACKAGE) > $(AOSP_TARGET_PACKAGE).md5sum
 	echo -e "========================================="
-	echo -e ""
-	echo -e	"		 __ _  ___  ___ _ __  
- 	echo -e "		/ _` |/ _ \/ __| '_ \ 
-	echo -e	"	    | (_| | (_) \__ \ |_) |
- 	echo -e	"		\__,_|\___/|___/ .__/ 
-	echo -e "               		|_|
-	echo -e	"
-	echo -e ""
+	echo -e "    AOSP  "
+	echo -e "    AOSP  "
+	echo -e "    AOSP  "
+	echo -e "    AOSP  "
+	echo -e "    AOSP  "
 	echo -e "========================================="
 	echo -e ""
 	@echo "**Package completed**: $(AOSP_TARGET_PACKAGE)"
